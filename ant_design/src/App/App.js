c238c6bddd0cf3fa1dffed1d64e40c785399a3df
jiangwenyong <jiangwenyong@qq.com>
2021-10-02 23:59:29 +0800

    added: 增加ant-design项目

---
+++ ant_design/src/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from 'antd';
+import { HomeOutlined } from '@ant-design/icons';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Primary Button</Button>
+      <HomeOutlined />
+    </div>
+  );
+}
+
+export default App;
